1aa87ade9faae66a242dfc353528ff6438f72bda
Michael Fester <michael.fester@gmail.com>
2017-09-20 18:36:52 +0200

    Sample skill

---
+++ myskill/myskill.py
@@ -0,0 +1,18 @@
+# -*-: coding utf-8 -*-
+""" Skeleton Snips skill. """
+
+class MySkill:
+    """ Skeleton Snips skill. """
+
+    def __init__(self, hostname, api_key):
+        """
+        :param hostname: hostname for some IoT device
+        :param api_key: API key for some service
+        """
+        self.hostname = hostname
+        self.username = username
+        self.light_ids = light_ids
+
+    def turn_on(self):
+        """ Turn on something. """
+        print("Hello!")
