228f279c2800ba9189ad967acfc58301a7e923e1
Michael Fester <michael.fester@gmail.com>
2017-09-21 09:43:54 +0200

    Update myskill.py

--- myskill/myskill.py
+++ myskill/myskill.py
@@ -4,15 +4,22 @@
 class MySkill:
     """ Skeleton Snips skill. """
 
-    def __init__(self, hostname, api_key):
+    def __init__(self, hostname, light_ids):
         """
         :param hostname: hostname for some IoT device
-        :param api_key: API key for some service
+        :param light_ids: A list of light IDs
         """
         self.hostname = hostname
-        self.username = username
         self.light_ids = light_ids
 
     def turn_on(self):
         """ Turn on something. """
-        print("Hello!")
+        print("Turn on")
+        
+    def turn_off(self):
+        """ Turn of something. """
+        print("Turn off")
+
+    def set_color_name(self, object_color):
+        """ Set an object color. """
+        print("Set object color to {}".format(object_color))
